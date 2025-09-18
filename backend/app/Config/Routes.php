a52c2386fc50f2d227e3ced75349090baa92d134
kulit129163 <martincliffordore16@gmail.com>
2025-09-18 22:12:34 +0800

    Initialize Project-1/hot-fix/header

---
+++ backend/app/Config/Routes.php
@@ -0,0 +1,24 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+// User exposed pages
+$routes->get('/', 'Users::index');
+$routes->get('mood-board', 'Users::moodBoard');
+$routes->get('road-map', 'Users::roadMap');
+
+// Auth
+$routes->get('login', 'Auth::showLogin');
+$routes->post('login', 'Auth::login');
+$routes->get('logout', 'Auth::logout');
+$routes->get('signup', 'Auth::showSignup');
+$routes->post('signup', 'Auth::signup');
+
+// Admin:Manager
+$routes->get('admin/dashboard', 'Admin::dashboard');
+
+// Debug
+$routes->get('auth-debug', 'Auth::debugCheck');
